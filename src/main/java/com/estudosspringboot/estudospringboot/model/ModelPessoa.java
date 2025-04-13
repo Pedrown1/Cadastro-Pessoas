6f820e7f06ff4c0782d0b200624f81dfd37a817a
Pedro Henrique <phpedrohc1967@gmail.com>
2025-04-13 11:53:11 -0300

    Integrando Banco de Dados Oracle

--- src/main/java/com/estudosspringboot/estudospringboot/model/ModelPessoa.java
+++ src/main/java/com/estudosspringboot/estudospringboot/model/ModelPessoa.java
@@ -1,14 +1,12 @@
 package com.estudosspringboot.estudospringboot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.ToString;
 
 @Entity
 @Data
+@Table(name = "pessoa")
 @ToString
 public class ModelPessoa {
     @Id
@@ -20,48 +18,6 @@ public class ModelPessoa {
     private int idade;
 
     private String cpf;
-
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getNome() {
-//        return nome;
-//    }
-//
-//    public void setNome(String nome) {
-//        this.nome = nome;
-//    }
-//
-//    public int getIdade() {
-//        return idade;
-//    }
-//
-//    public void setIdade(int idade) {
-//        this.idade = idade;
-//    }
-//
-//    public String getCpf() {
-//        return cpf;
-//    }
-//
-//    public void setCpf(String cpf) {
-//        this.cpf = cpf;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "ModelPessoa{" +
-//                "id=" + id +
-//                ", nome='" + nome + '\'' +
-//                ", idade=" + idade +
-//                ", cpf='" + cpf + '\'' +
-//                '}';
-//    }
 
     // Lombok (GET/SET)
 }
